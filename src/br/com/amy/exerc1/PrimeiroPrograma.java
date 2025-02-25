36185ea48779928001a35e7a15e0a6dfe582265f
Amy Lee <amyleesouzatr@gmail.com>
2025-02-25 16:34:55 -0300

    Adicionando arquivos iniciais no projeto

---
+++ src/br/com/amy/exerc1/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package br.com.amy.exerc1;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Follow the white rabbit!");
+
+    }
+}
---
+++ src/br/com/amy/exerc3/Neo.java
@@ -0,0 +1,24 @@
+package br.com.amy.exerc3;
+
+/**
+ * @author Amy Lee <@teste@gmail.com>
+ * @since JDK 21
+ * Comentário para o java doc
+ */
+public class Neo {
+
+    // Para remover imports não utilizados, use o comando control+alt+o
+    // Este é um comentário de uma linha
+    // Qualquer texto - usei control-barra
+    // Para o código ser formatado, use control+alt+l
+    // Para comentar um java doc, uso /**
+
+    /*
+
+
+      Este é um comentário de várias linhas
+
+
+     */
+
+}
